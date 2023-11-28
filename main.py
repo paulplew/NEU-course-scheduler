447ecca56f05690d8b184c4030816b7292ea5285
paulplew <thepaulplew@gmail.com>
2023-11-28 11:37:36 -0500

    Alert the user if courses entered do not exist and remove those.

--- main.py
+++ main.py
@@ -4,10 +4,23 @@ print("Welcome to the NEU automatic course scheduler program.")
 classes = input("Please input the class codes for the classes you wish to take next semester in the following format: CS2500, ECON3100, ART1200 \n").upper()
 
 courses = []
+course_titles = []
 for course in classes.split(", "):
+    course_titles.append(course)
     course = [course[:-4], course[-4:]]
     courses.append(search_classes(course[0], course[1]))
-classes = courses
+    
+valid_courses = []
+valid_course_titles = []
+for course, title in zip(courses, course_titles):
+    # check if course is empty
+    if not course:
+        print(f"The course {title} does not exist. Continuing without that course.")
+    else:
+        valid_courses.append(course) 
+        valid_course_titles.append(title) 
+courses = valid_courses
+course_titles = valid_course_titles
 
 time_pref = input("Please enter m, a, or e to signift whether you'd prefer morning, afternoon, or evening classes.\n").lower()
 while (time_pref != 'm' and time_pref != 'a' and time_pref != 'e'):
